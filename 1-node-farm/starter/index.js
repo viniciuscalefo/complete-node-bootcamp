81f673fd7b4e80e497deee9ea48ae0d3ce795da7
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2024-07-23 08:45:18 -0300

    .

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -35,9 +35,25 @@ const url = require('url');
 ////////////////////////////////////////////////////
 //SERVER
 //First we created the server and sevond we use starserver, to ear incoming request
+const replaceTemplate = (temp,product)=>{
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%ID%}/g, product.id)
 
-const data = fs.readFileSync('utf-8', (err,data)=>{
-});
+  if(!product.organic)  output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+    return output
+}
+
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data)
 
 
@@ -45,13 +61,28 @@ const dataObj = JSON.parse(data)
 const server = http.createServer((req,res) => { //creating the server
   const pathName = req.url;
 
-  if(pathName === '/' || pathName  === '/overview'){
-    res.end = ('Yhis is a OVERVIEW')
-  }else if(pathName === '/' || pathName === '/product'){
+  //Overview Page
+  if(pathName === '/'){
+    res.writeHead(200,{'Content-type':'text/html'})
+
+    const cardsHtml = dataObj.map(el=> replaceTemplate(tempCard,el))
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
+
+    res.end = (output)
+
+
+    //Product Page
+  }else if( pathName === '/product'){
     res.end(' This is a Product')
+
+
+     //API 
   }else if (pathName === '/api'){
       res.writeHead(200,{'Content-type':'application/json'})
-      res.end(dataObj)
+      res.end(data)
+
+
+  //Not Found Page
   }else{
     res.writeHead(404,{
       'Content-type':'text/html',
