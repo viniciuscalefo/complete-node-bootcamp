6df4855d61ba2e66992b0fff3241def8a88400e3
Vinicius Calefo Assarice <116103355+viniciuscalefo@users.noreply.github.com>
2024-07-24 08:41:11 -0300

    .

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const http = require('http');
+const path = require('path');
 const { dirname } = require('path');
 const url = require('url');
 
@@ -35,26 +36,26 @@ const url = require('url');
 ////////////////////////////////////////////////////
 //SERVER
 //First we created the server and sevond we use starserver, to ear incoming request
-const replaceTemplate = (temp,product)=>{
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-  output = output.replace(/{%IMAGE%}/g, product.image)
-  output = output.replace(/{%PRICE%}/g, product.price)
-  output = output.replace(/{%FROM%}/g, product.from)
-  output = output.replace(/{%QUANTITY%}/g, product.quantity)
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-  output = output.replace(/{%DESCRIPTION%}/g, product.description)
-  output = output.replace(/{%ID%}/g, product.id)
+// const replaceTemplate = (temp,product)=>{
+//   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+//   output = output.replace(/{%IMAGE%}/g, product.image)
+//   output = output.replace(/{%PRICE%}/g, product.price)
+//   output = output.replace(/{%FROM%}/g, product.from)
+//   output = output.replace(/{%QUANTITY%}/g, product.quantity)
+//   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
+//   output = output.replace(/{%DESCRIPTION%}/g, product.description)
+//   output = output.replace(/{%ID%}/g, product.id)
 
-  if(!product.organic)  output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-    return output
-}
+//   if(!product.organic)  output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+//     return output
+// }
 
 
-const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-const dataObj = JSON.parse(data)
+// const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+// const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+// const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+// const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+// const dataObj = JSON.parse(data)
 
 
 
@@ -64,13 +65,14 @@ const server = http.createServer((req,res) => { //creating the server
   const pathName = req.url;
 
   //Overview Page
-  if(pathName === '/home'){
-    res.writeHead(200,{'Content-type':'text/html'})
+  if(pathName === '/' || pathName === '/overview'){
+    res.end('Overview Page')
+    // res.writeHead(200,{'Content-type':'text/html'})
 
-    const cardsHtml = dataObj.map(el=> replaceTemplate(tempCard,el))
-    const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
+    // const cardsHtml = dataObj.map(el=> replaceTemplate(tempCard,el))
+    // const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
 
-    res.end = output
+    // res.end = output
 
 
     //Product Page
@@ -88,9 +90,9 @@ const server = http.createServer((req,res) => { //creating the server
   }else{
     res.writeHead(404,{
       'Content-type':'text/html',
-      'my-own-header':'hello world'
+      'my-own-header':'hello-world'
     })
-    res.end('Page not found')
+    res.end('<h1>Page not found</h1>')
   }
 })
 
