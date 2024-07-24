3940882b99998c135e837d0cffeaee0bf2c0bad8
Vinicius Calefo <vincalefo@gmail.com>
2024-07-24 15:35:49 -0300

    .
    .

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 const http = require('http');
-const path = require('path');
+// const path = require('path');
 const { dirname } = require('path');
 const url = require('url');
 
 ////////////////////////////////////////////////////
 //FILES
-
-
 //BLOCKING SYNCHONOS WAY
 // const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 // console.log(textIn)
@@ -35,56 +33,57 @@ const url = require('url');
 
 ////////////////////////////////////////////////////
 //SERVER
-//First we created the server and sevond we use starserver, to ear incoming request
-// const replaceTemplate = (temp,product)=>{
-//   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
-//   output = output.replace(/{%IMAGE%}/g, product.image)
-//   output = output.replace(/{%PRICE%}/g, product.price)
-//   output = output.replace(/{%FROM%}/g, product.from)
-//   output = output.replace(/{%QUANTITY%}/g, product.quantity)
-//   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients)
-//   output = output.replace(/{%DESCRIPTION%}/g, product.description)
-//   output = output.replace(/{%ID%}/g, product.id)
+//First we created the server and second we use starserver, to ear incoming request
 
-//   if(!product.organic)  output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
-//     return output
-// }
+const replaceTemplate = (temp,product)=>{
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
 
+  if(!product.organic)  output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
 
-// const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-// const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
-// const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
-// const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
-// const dataObj = JSON.parse(data)
+//Loading Templates
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
-
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data)
 
 const server = http.createServer((req,res) => { //creating the server
-  // console.log(req.url)
-  // console.log(req.parse(req.url,true))
-  const pathName = req.url;
+
+  const {query,pathname} = url.parse(req.url, true)
 
   //Overview Page
-  if(pathName === '/' || pathName === '/overview'){
-    res.end('Overview Page')
-    // res.writeHead(200,{'Content-type':'text/html'})
+  if(pathname === '/' || pathname === '/overview'){
+    res.writeHead(200,{'Content-type':'text/html'})
+    
+    const cardsHtml = dataObj.map(el=> replaceTemplate(tempCard , el)).join('')
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
 
-    // const cardsHtml = dataObj.map(el=> replaceTemplate(tempCard,el))
-    // const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
-
-    // res.end = output
-
+    res.end(output)
 
     //Product Page
-  }else if( pathName === '/product'){
-    res.end(' This is a Product')
+  }else if( pathname === '/product'){
+    res.writeHead(200,{'Content-type':'text/html'})
+    const product = dataObj[query.id]
+    const output = replaceTemplate(tempProduct,product)
+
+    res.end(output)
 
 
      //API 
-  }else if (pathName === '/api'){
-      res.writeHead(200,{'Content-type':'application/json'})
-      res.end(data)
+  }else if (pathname === '/api'){
 
+      res.writeHead(200, {'Content-type':'application/json'})
+      res.end(data)
 
   //Not Found Page
   }else{
